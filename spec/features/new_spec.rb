6f5ad5a3b73653a18f633badaa1e0afffd5d546d
michael.budnick <michael.budnick@outlook.com>
2015-12-18 15:10:56 -0500

    Finish new form test

--- spec/features/new_spec.rb
+++ spec/features/new_spec.rb
@@ -42,4 +42,33 @@ RSpec.feature 'New Product form' do
       expect(page).to have_content("Wine Type:")
     end
 
+    it "creates a new wine on valid input" do
+      fill_in 'Wine name:', :with => "TestName", visible: false
+      fill_in 'Wine URL:', :with => "www.testurl.com", visible: false
+      fill_in 'Wine Max Price:', :with => "34.00", visible: false
+      fill_in 'Wine Min Price:', :with => "32.00", visible: false
+      fill_in 'Wine Retail Price:', :with => "40.00", visible: false
+      select "Wine"
+
+      click_button 'Submit'
+      expect(page).to have_content('All Wines')
+      expect(page).to have_content('TestName')
+    end
+
+    it "handles validation errors for presence" do
+      click_button 'Submit'
+      expect(page).to have_content("can't be blank", count: 6)
+    end
+
+    it "handles validation errors for max/min" do
+      fill_in 'Wine name:', :with => "TestName", visible: false
+      fill_in 'Wine URL:', :with => "www.testurl.com", visible: false
+      fill_in 'Wine Max Price:', :with => "32.00", visible: false
+      fill_in 'Wine Min Price:', :with => "34.00", visible: false
+      fill_in 'Wine Retail Price:', :with => "40.00", visible: false
+      select "Wine"
+
+      click_button 'Submit'
+      expect(page).to have_content('Max price must be greater than or equal to')
+    end
 end
