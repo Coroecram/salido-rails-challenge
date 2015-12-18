91bcae9ae3eb94bdf2fde6ff5435ebbca313be01
michael.budnick <michael.budnick@outlook.com>
2015-12-18 10:14:48 -0500

    Interacting with WineApi

--- app/services/api_fetch.rb
+++ app/services/api_fetch.rb
@@ -3,37 +3,34 @@ class APIFetch
 
   def initialize(search_query)
     @wines = []
-    @errors = ""
-    @types = search_query[:kind].gsub(" ", "+")
-    @search_params = search_query[:query]
+    @errors = []
+    @search_params = search_query[:query].gsub(" ", "+")
+    @types = search_query[:kind]
     @price_min =  search_query[:price_min]
     @price_max =  search_query[:price_max]
     debugger
 
     validate
-    query = parse_query
-    fetch_wines(query)
+    @query_string = parse_query
+    fetch_wines
   end
 
   def validate
-    if @price_max.to_i >= price_min.to_i
-      @errors = "Max price must be greater than min price"
-    elsif @price_max == 0 && @price_min == 0
-
+    if @price_max >= @price_min && @price_max != "" || @price_min != ""
+      @errors.push("Max price must be greater than min price")
     end
   end
 
   def parse_query
-    types = @types != ? "search=#{@types}&" : ""
-    if @price_min || @price_max
-      min = @price_min != "" ? @price_min : "0"
-      max = @price_max != "" ? @price_max : "9999"
-      filter = "filter="
-    return "#{types}"
+    types = @types != "" ? "search=#{@search_params}&" : ""
+    min = @price_min != "" ? @price_min : 0
+    max = @price_max != "" ? @price_max : 9999
+    filter = "filter=#{min}|#{max}&"
+    return "#{types}#{filter}"
   end
 
   def fetch_wines
-    wines = JSON.parse(RestClient.get "http://services.wine.com/api/beta2/service.svc/json/catalog?offset=5&size=5&state=NY&apikey=#{ENV['WINE_API_KEY']}")["Products"]["List"]
+    wines = JSON.parse(RestClient.get "http://services.wine.com/api/beta2/service.svc/json/catalog?#{@query_string}offset=0&size=5&state=NY&apikey=#{ENV['WINE_API_KEY']}")["Products"]["List"]
     debugger
     # wines = json_wines
     # json_wines.each { |wine| Wine.create!({name: wine["Name"],
