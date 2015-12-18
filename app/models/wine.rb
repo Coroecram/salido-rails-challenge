72a633a91b09d1b7bd7b3ace584ab197bae34960
michael.budnick <michael.budnick@outlook.com>
2015-12-18 11:02:59 -0500

    Add validation on create that max is greater than min

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,4 +1,6 @@
 class Wine < ActiveRecord::Base
   validates :name, :url, :min_price, :max_price, :retail_price, :kind, presence: true
   validates :name, :url, uniqueness: true
+  validates_numericality_of :min_price, :retail_price
+  validates_numericality_of :max_price, :greater_than => :min_price
 end
