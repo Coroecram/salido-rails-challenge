0e49f7cab4e803fd7a897db48da6c85b75edb650
michael.budnick <michael.budnick@outlook.com>
2015-12-18 06:38:43 -0500

    Create Wines table

--- db/migrate/20151218113051_create_wines.rb
+++ db/migrate/20151218113051_create_wines.rb
@@ -3,13 +3,19 @@ class CreateWines < ActiveRecord::Migration
     create_table :wines do |t|
       t.string :name
       t.string :url
-      t.decimal :pricemin
-      t.decimal :pricemax
-      t.decimal :priceretail
+      t.decimal :min_price
+      t.decimal :max_price
+      t.decimal :retail_price
       t.string :type
       t.string :year
 
       t.timestamps null: false
     end
+    add_index :wines, :name
+    add_index :wines, :year
+    add_index :wines, :type
+    add_index :wines, :min_price
+    add_index :wines, :max_price
+    add_index :wines, :retail_price
   end
 end
