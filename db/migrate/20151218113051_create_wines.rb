13720acf37054a84c092d67566dc64278eafbc2f
michael.budnick <michael.budnick@outlook.com>
2015-12-18 07:19:17 -0500

    Populate database from API with 100 wines

--- db/migrate/20151218113051_create_wines.rb
+++ db/migrate/20151218113051_create_wines.rb
@@ -6,16 +6,16 @@ class CreateWines < ActiveRecord::Migration
       t.decimal :min_price
       t.decimal :max_price
       t.decimal :retail_price
-      t.string :type
-      t.string :year
+      t.string :kind
+      t.integer :api_id
 
       t.timestamps null: false
     end
     add_index :wines, :name
-    add_index :wines, :year
-    add_index :wines, :type
+    add_index :wines, :kind
     add_index :wines, :min_price
     add_index :wines, :max_price
     add_index :wines, :retail_price
+    add_index :wines, :api_id
   end
 end
