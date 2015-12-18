167910032b6d992a4c2d17aec1050c07e780d79b
michael.budnick <michael.budnick@outlook.com>
2015-12-18 10:33:40 -0500

    API fetch creating or finding unique wines

--- app/services/api_fetch.rb
+++ app/services/api_fetch.rb
@@ -9,7 +9,7 @@ class APIFetch
     @price_min =  search_query[:price_min] != "" ? @price_min : 0
     @price_max =  search_query[:price_max] != "" ? @price_max : 9999
     @size = search_query[:size] != "" ? search_query[:size] : 5
-    @query_string = "search=#{@search_params}&filter=#{@price_min}|#{@price_max}&offset=0&size=#{@size}&state=NY&apikey=#{ENV['WINE_API_KEY']}"
+    @query_string = "search=#{@search_params}&filter=price(#{@price_min}|#{@price_max})&offset=0&size=#{@size}&state=NY&apikey=#{ENV['WINE_API_KEY']}"
 
     validate
     fetch_wines
@@ -24,14 +24,14 @@ class APIFetch
   def fetch_wines
     debugger
     wines = JSON.parse(RestClient.get "http://services.wine.com/api/beta2/service.svc/json/catalog?#{@query_string}")["Products"]["List"]
-    wines.each { |wine| @wines.push(Wine.find_or_create_by({name: wine["Name"],
+    wines.each { |wine| @wines.push(Wine.find_or_create_by!({name: wine["Name"],
                                                             url: wine["Url"],
                                                             min_price: wine["PriceMin"],
                                                             max_price: wine["PriceMax"],
                                                             retail_price: wine["PriceRetail"],
                                                             kind: wine["Type"],
                                                             api_id: wine["Id"] })
-                                                          ) }
+                                                          )}
   end
 
 
