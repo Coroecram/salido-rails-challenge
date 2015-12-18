d88b3b1bf10fd1589749aaef4170afd2b3f31594
michael.budnick <michael.budnick@outlook.com>
2015-12-18 13:26:15 -0500

    Added feature testing for API retrieval

--- app/services/api_fetch.rb
+++ app/services/api_fetch.rb
@@ -9,7 +9,7 @@ class APIFetch
     @price_min =  search_query[:price_min] != "" ? search_query[:price_min] : 0
     @price_max =  search_query[:price_max] != "" ? search_query[:price_max] : 9999
     @size = search_query[:size] != "" ? search_query[:size] : 5
-    @query_string = "search=#{@search_params}&filter=price(#{@price_min}|#{@price_max})&offset=0&size=#{@size}&state=NY&#{ENV['WINE_API_KEY']}"
+    @query_string = "search=#{@search_params}&filter=price(#{@price_min}|#{@price_max})&offset=300&size=#{@size}&state=NY&#{ENV['WINE_API_KEY']}"
 
     validate
     fetch_wines if @errors.empty?
@@ -23,14 +23,18 @@ class APIFetch
 
   def fetch_wines
     wines = JSON.parse(RestClient.get "http://services.wine.com/api/beta2/service.svc/json/catalog?#{@query_string}")["Products"]["List"]
-    wines.each { |wine| @wines.push(Wine.find_or_create_by!({name: wine["Name"],
-                                                            url: wine["Url"],
-                                                            min_price: wine["PriceMin"],
-                                                            max_price: wine["PriceMax"],
-                                                            retail_price: wine["PriceRetail"],
-                                                            kind: wine["Type"],
-                                                            api_id: wine["Id"] })
-                                                          )}
+
+    wines.each do |wine|
+      @wines.push(Wine.find_or_create_by!({name: wine["Name"],
+                                           url: wine["Url"],
+                                           min_price: wine["PriceMin"],
+                                           max_price: wine["PriceMax"],
+                                           retail_price: wine["PriceRetail"],
+                                           kind: wine["Type"],
+                                           api_id: wine["Id"] })
+                                          )
+    end
+
   end
 
 
--- spec/features/api_form_spec.rb
+++ spec/features/api_form_spec.rb
@@ -1,3 +1,5 @@
+require 'spec_helper'
+
 RSpec.feature 'Api requests' do
 
   before(:each) { visit wines_api_form_path }
@@ -6,7 +8,39 @@ RSpec.feature 'Api requests' do
     expect(page).to have_content('Find Wines')
   end
 
-  
+  it 'handles finding nothing' do
+    fill_in 'Search by wine name:', :with => "zzzyyyxxxtttzzz34", visible: false
+    click_button 'Submit'
 
+    expect(page).to have_content('That query did not return any wines')
+  end
+
+  it 'handles search errors' do
+    fill_in 'From:', :with => 9999, visible: false
+    fill_in 'To:', :with => 0, visible: false
+    click_button 'Submit'
+
+    expect(page).to have_content('Max price must be greater than min price')
+  end
+
+  it "retrieves specified amount" do
+    fill_in 'Amount to return:', :with => 7, visible: false
+    click_button 'Submit'
+
+    expect(page).to have_selector('li', count: 7)
+  end
+
+  it 'searches correctly' do
+    fill_in 'Search by wine name:', :with => "Cabernet", visible: false
+    click_button 'Submit'
+
+    expect(page).to have_content('Cabernet', count: 5)
+  end
+
+  it 'redirects on success' do
+    click_button 'Submit'
+
+    expect(page).to have_content('Retrieved Wines')
+  end
 
 end
